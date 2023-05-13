269499eecb70509b8e6fad5524c3093bf512f81b
clarkjensen <clark.jensen@live.com>
2023-05-12 21:55:09 -0700

    updated to find relationships between HR search query parameters

--- scripts/loadAndTestModel.py
+++ scripts/loadAndTestModel.py
@@ -1,16 +1,80 @@
 import gensim
 import os
+import sys
+
+# switch these and the tests when switching which model to test (hotel reviews or HR search queries)
+# dataset_file = "../reviews_data.txt.gz"
+dataset_file = "../search-results-by-url.csv"
+
+# Split the file location into directory path and file name with extension
+dir_path, file_name_ext = os.path.split(dataset_file)
+
+# Split the file name and extension
+file_name, file_ext = os.path.splitext(file_name_ext)
 
 abspath = os.path.dirname(os.path.abspath(__file__))
-reloaded_word_vectors = gensim.models.KeyedVectors.load(os.path.join(abspath, "../vectors/vectors.kv"))
 
-w1 = "dirty"
+reloaded_word_vectors = gensim.models.KeyedVectors.load(os.path.join(abspath, "../vectors/" + file_name + ".kv"))
+
+w1 = "q.anyPlace=Newcastle%20upon%20tyne"
 print("Most similar to {0}".format(w1), reloaded_word_vectors.most_similar(positive=w1))
+print("Least similar to {0}".format(w1), list(reversed(reloaded_word_vectors.most_similar("q.anyPlace=Newcastle%20upon%20tyne", topn=sys.maxsize)[-10:])))
 
-# look up top 6 words similar to 'polite'
-w1 = ["polite"]
-print(
-    "Most similar to {0}".format(w1),
-    reloaded_word_vectors.most_similar(
-        positive=w1,
-        topn=6))
+
+# w1 = "dirty"
+# print("Most similar to {0}".format(w1), reloaded_word_vectors.most_similar(positive=w1))
+
+# # look up top 6 words similar to 'polite'
+# w1 = ["polite"]
+# print(
+#     "Most similar to {0}".format(w1),
+#     reloaded_word_vectors.most_similar(
+#         positive=w1,
+#         topn=6))
+
+# # look up top 6 words similar to 'france'
+# w1 = ["france"]
+# print(
+#     "Most similar to {0}".format(w1),
+#     reloaded_word_vectors.most_similar(
+#         positive=w1,
+#         topn=6))
+
+# # look up top 6 words similar to 'shocked'
+# w1 = ["shocked"]
+# print(
+#     "Most similar to {0}".format(w1),
+#     reloaded_word_vectors.most_similar(
+#         positive=w1,
+#         topn=6))
+
+# # look up top 6 words similar to 'shocked'
+# w1 = ["beautiful"]
+# print(
+#     "Most similar to {0}".format(w1),
+#     reloaded_word_vectors.most_similar(
+#         positive=w1,
+#         topn=6))
+
+# # get everything related to stuff on the bed
+# w1 = ["bed", 'sheet', 'pillow']
+# w2 = ['couch']
+# print(
+#     "Most similar to {0}".format(w1),
+#     reloaded_word_vectors.most_similar(
+#         positive=w1,
+#         negative=w2,
+#         topn=10))
+
+# # similarity between two different words
+# print("Similarity between 'dirty' and 'smelly'",
+#       reloaded_word_vectors.similarity(w1="dirty", w2="smelly"))
+
+# # similarity between two identical words
+# print("Similarity between 'dirty' and 'dirty'",
+#       reloaded_word_vectors.similarity(w1="dirty", w2="dirty"))
+
+# # similarity between two unrelated words
+# print("Similarity between 'dirty' and 'clean'",
+#       reloaded_word_vectors.similarity(w1="dirty", w2="clean"))
+
--- scripts/word2vec.py
+++ scripts/word2vec.py
@@ -1,38 +1,97 @@
+import os
+import logging
+import pandas as pd
 import gzip
 import gensim
-import logging
-import os
 
 logging.basicConfig(
     format='%(asctime)s : %(levelname)s : %(message)s',
     level=logging.INFO)
 
+# switch between these to build a model either on hotel reviews, or on HR search queries
+# dataset_file = "../reviews_data.txt.gz"
+dataset_file = "../search-results-by-url.csv"
 
 def show_file_contents(input_file):
-    with gzip.open(input_file, 'rb') as f:
-        for i, line in enumerate(f):
-            print(line)
-            break
+    df = pd.read_csv(input_file, encoding='utf-8', header=0, dtype=str)
+    print(df.head(20))
 
 
 def read_input(input_file):
-    """This method reads the input file which is in gzip format"""
+    """This method reads the input file which can be in csv, tsv, txt, or gzip format."""
 
+    file_extension = os.path.splitext(input_file)[1]
     logging.info("reading file {0}...this may take a while".format(input_file))
-    with gzip.open(input_file, 'rb') as f:
-        for i, line in enumerate(f):
 
-            if (i % 10000 == 0):
-                logging.info("read {0} reviews".format(i))
-            # do some pre-processing and return list of words for each review
-            # text
-            yield gensim.utils.simple_preprocess(line)
+    if file_extension == '.csv':
+        df = pd.read_csv(input_file, encoding='utf-8', header=0, dtype=str)
+        df.dropna(inplace=True)  # remove rows with missing data
+        # for review in df.iloc[:, 0]:
+        for i, review in enumerate(df.iloc[:, 0]):
+            if "?" in review:
+                if (i % 1000 == 0):
+                    logging.info("read {0} lines".format(i))
+
+                # this is specific to the HR Search queries -- not applicable to all csv files
+                query_params = review.split("?")[1]
+                query_params = query_params.replace("&", " ")
+
+                # this is a temporary solution -- we should use the simple_preprocess, but it doesn't
+                # work with things like symbols or numbers (like those in HR Search queries).  It 
+                # improves the training speed if we use it.
+                query_params = query_params.split(" ")
+                yield query_params
+                # yield gensim.utils.simple_preprocess(query_params)
+
+    elif file_extension == '.tsv':
+        df = pd.read_csv(input_file, delimiter='\t', encoding='utf-8', header=0, dtype=str)
+        df.dropna(inplace=True)  # remove rows with missing data
+        for i, review in enumerate(df.iloc[:, 0]):
+            if "?" in review:
+                if (i % 1000 == 0):
+                    logging.info("read {0} lines".format(i))
+
+                # this is specific to the HR Search queries -- not applicable to all csv files
+                query_params = review.split("?")[1]
+                query_params = query_params.replace("&", " ")
+
+                # this is a temporary solution -- we should use the simple_preprocess, but it doesn't
+                # work with things like symbols or numbers (like those in HR Search queries).  It 
+                # improves the training speed if we use it.
+                query_params = query_params.split(" ")
+                yield query_params
+                # yield gensim.utils.simple_preprocess(query_params)
+
+    elif file_extension == '.txt':
+        with open(input_file, 'r', encoding='utf-8') as f:
+            for i, line in enumerate(f):
+
+                if (i % 10000 == 0):
+                    logging.info("read {0} reviews".format(i))
+                # do some pre-processing and return list of words for each review
+                # text
+                yield gensim.utils.simple_preprocess(line)
+
+    elif file_extension == '.gz':
+        with gzip.open(input_file, 'rb') as f:
+            for i, line in enumerate(f):
+
+                if (i % 10000 == 0):
+                    logging.info("read {0} reviews".format(i))
+                # do some pre-processing and return list of words for each review
+                # text
+                yield gensim.utils.simple_preprocess(line)
+
+    else:
+        logging.error("unsupported file type: {0}".format(file_extension))
+
+
 
 
 if __name__ == '__main__':
 
     abspath = os.path.dirname(os.path.abspath(__file__))
-    data_file = os.path.join(abspath, "../reviews_data.txt.gz")
+    data_file = os.path.join(abspath, dataset_file)
 
     # read the tokenized reviews into a list
     # each review item becomes a serries of words
@@ -47,64 +106,14 @@ if __name__ == '__main__':
         window=10,
         min_count=2,
         workers=10)
-    model.train(documents, total_examples=len(documents), epochs=1)
 
-    # save only the word vectors
-    model.wv.save(os.path.join(abspath, "../vectors/vectors.kv"))
+    model.train(documents, total_examples=len(documents), epochs=10)
 
-    w1 = "dirty"
-    print("Most similar to {0}".format(w1), model.wv.most_similar(positive=w1))
+    # Split the file location into directory path and file name with extension
+    dir_path, file_name_ext = os.path.split(dataset_file)
 
-    # look up top 6 words similar to 'polite'
-    w1 = ["polite"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
+    # Split the file name and extension
+    file_name, file_ext = os.path.splitext(file_name_ext)
 
-    # look up top 6 words similar to 'france'
-    w1 = ["france"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
-
-    # look up top 6 words similar to 'shocked'
-    w1 = ["shocked"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
-
-    # look up top 6 words similar to 'shocked'
-    w1 = ["beautiful"]
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            topn=6))
-
-    # get everything related to stuff on the bed
-    w1 = ["bed", 'sheet', 'pillow']
-    w2 = ['couch']
-    print(
-        "Most similar to {0}".format(w1),
-        model.wv.most_similar(
-            positive=w1,
-            negative=w2,
-            topn=10))
-
-    # similarity between two different words
-    print("Similarity between 'dirty' and 'smelly'",
-          model.wv.similarity(w1="dirty", w2="smelly"))
-
-    # similarity between two identical words
-    print("Similarity between 'dirty' and 'dirty'",
-          model.wv.similarity(w1="dirty", w2="dirty"))
-
-    # similarity between two unrelated words
-    print("Similarity between 'dirty' and 'clean'",
-          model.wv.similarity(w1="dirty", w2="clean"))
+    # save only the word vectors, and save it to a file that is similarly named to the source file
+    model.wv.save(os.path.join(abspath, "../vectors/" + file_name + ".kv"))
